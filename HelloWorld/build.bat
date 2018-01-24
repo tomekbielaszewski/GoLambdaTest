81901276fb5e65c4c19577e27977ff0223a62491
tbielaszewski <tomekbielaszewski@gmail.com>
2018-01-24 11:22:45 +0100

    deployment package rename, some changes in build script

--- HelloWorld/build.bat
+++ HelloWorld/build.bat
@@ -1,6 +1,7 @@
 REM You need to install the tool below only once
 REM go.exe get -u github.com/aws/aws-lambda-go/cmd/build-lambda-zip
 
+set GOARCH=amd64
 set GOOS=linux
-go build -o main main.go
-%GOPATH%\bin\build-lambda-zip.exe -o deployment2.zip main
+go build -x -o main main.go
+%GOPATH%\bin\build-lambda-zip.exe -o deployment.zip main
